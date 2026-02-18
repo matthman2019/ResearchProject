597f3c87e384ce9b215e297ea64994fb8072ebed
matthman2019 <matthman2019@gmail.com>
2026-02-18 14:21:38 -0500

    Final Figure 3!
    Added final figure 3.
    Shrank axis tick labels.

--- DataAnalysis/analysis.py
+++ DataAnalysis/analysis.py
@@ -27,7 +27,7 @@ def openFile(filePath : Path) -> dict:
 
     return returnDict
 
-graphHeight = 10
+graphHeight = 100
 def loadData(graph, filename : Path, language : str, color : str):
     global graphHeight
     loadedDictionary = openFile(filename)
@@ -38,8 +38,8 @@ def loadData(graph, filename : Path, language : str, color : str):
     graph.bar(list(loadedDictionary.keys()), meanList, yerr=stdErrList, color=color)
     graph.set_title(f"{language} Output")
     graph.set_ylim(0, graphHeight)
-
-    print(f"{language}: {stdErrList}")
+    graph.tick_params(axis='both', labelsize=6)
+    print(f"{language}: {meanList}")
 
 colorDict = {
     "Python" : "blue",
@@ -55,6 +55,7 @@ colorDict = {
 }
 
 if __name__ == "__main__":
+    
     fig, ((ax1, ax2, ax3, ax4, ax5), (ax6, ax7, ax8, ax9, ax10)) = plt.subplots(2, 5)
     axList = [ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10]
     fileList : list[Path] = []
@@ -67,56 +68,5 @@ if __name__ == "__main__":
     for file, graph in zip(fileList, axList):
         languageName = file.name.removesuffix("Output.txt")
         loadData(graph, file, languageName, colorDict[languageName])
-
-    '''
-    pythonDict = openFile(Path("PythonOutput2.txt"))
-    javascriptDict = openFile(Path("JavascriptOutput2.txt"))
-    luaDict = openFile(Path("LuaOutput2.txt"))
-    CPPDict = openFile(Path("C++Output2.txt"))
-    javaDict = openFile(Path("JavaOutput2.txt"))
-    rustDict = openFile(Path("RustOutput2.txt"))
-    goDict = openFile(Path("GoOutput2.txt"))
-    perlDict = openFile(Path("PerlOutput2.txt"))
-
-    graphHeight = 100
-    
-    ax1.bar(list(pythonDict.keys()), list(map(mean, pythonDict.values())), color="blue")
-    ax1.set_title("Python Output")
-    ax1.set_ylim(0, graphHeight)
-    ax2.bar(list(javascriptDict.keys()), list(map(mean, javascriptDict.values())), color="red")
-    ax2.set_title("Javascript Output")
-    ax2.set_ylim(0, graphHeight)
-    ax3.bar(list(luaDict.keys()), list(map(mean, luaDict.values())), color="skyblue")
-    ax3.set_title("Lua Output")
-    ax3.set_ylim(0, graphHeight)
-    ax4.bar(list(CPPDict.keys()), list(map(mean, CPPDict.values())), color="green")
-    ax4.set_title("C++ Output")
-    ax4.set_ylim(0, graphHeight)
-    ax5.bar(list(javaDict.keys()), list(map(mean, javaDict.values())), color="orange")
-    ax5.set_title("Java Output")
-    ax5.set_ylim(0, graphHeight)
-    ax6.bar(list(rustDict.keys()), list(map(mean, rustDict.values())), color="brown")
-    ax6.set_title("Rust Output")
-    ax6.set_ylim(0, graphHeight)
-    ax7.bar(list(goDict.keys()), list(map(mean, goDict.values())), color="teal")
-    ax7.set_title("Go Output")
-    ax7.set_ylim(0, graphHeight)
-    ax8.bar(list(perlDict.keys()), list(map(mean, perlDict.values())), color="pink")
-    ax8.set_title("Perl Output")
-    ax8.set_ylim(0, graphHeight)
-    '''
-  
-    '''
-
-
-    print(list(map(mean, pythonDict.values())))
-    print(list(map(mean, javascriptDict.values())))
-    print(list(map(mean, luaDict.values())))
-    print(list(map(mean, CPPDict.values())))
-    print(list(map(mean, javaDict.values())))
-    print(list(map(mean, goDict.values())))
-    print(list(map(mean, rustDict.values())))
-    print(list(map(mean, perlDict.values())))
-    '''
 
     plt.show()
